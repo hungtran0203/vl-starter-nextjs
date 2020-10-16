f68b521a5612c012c1226ab2f0b8a89c322f4aca
Hung Tran <hungtran0203@gmail.com>
2020-10-16 14:48:22 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-          Powered by{' '}
+          Powered by{' @VL '}
       </footer>
     </div>
   )
